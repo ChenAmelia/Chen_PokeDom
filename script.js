c232b5f355aff9264d326d56d2a7550742205b13
Chen <liangchen_amelia@163.com>
2022-08-30 10:23:36 +0100

    commit

--- script.js
+++ script.js
@@ -2,25 +2,76 @@ import {
     pokemonArray
 } from "./data/pokemon.js"
 
-//to see if the array was added to new js file and html file
-console.log(pokemonArray)
 
 const cardContainer = document.querySelector('.card-container');
-console.log(cardContainer);
+
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(title) {
+        return title.toUpperCase();
+      }
+    );
+  }
 
 
 pokemonArray.forEach(poArray => {
-    //console.log(poArray.id);
-    //console.log(poArray.name);
-    //console.log(poArray.types);
 
     cardContainer.innerHTML +=
     `<div class="card">
         <img class="card__image" src="${poArray.sprite}"></img>
         <section class="card__content">
-          <h2 calss="card__heading">${poArray.name}<h2>
-          <h2 calss="card__text">${poArray.name} ${poArray.id} is a ${poArray.types} type pokemon.<h2>
+          <p calss="card__heading">${toTitleCase(poArray.name)}</p>
+          <p calss="card__text">${poArray.name} ${poArray.id} is a ${poArray.types} type pokemon.</p>
         </section>
     </div>`
     });
+
+    //const nameInput = document.getElementById('searchByName')
+    //const typeInput = document.getElementById('searchByType')
+    const searchButton = document.getElementById('search')
+    const clearButton = document.getElementById('clear')
+    let filteredArr = [...pokemonArray]
+
+    console.log(nameInput);
+    console.log(typeInput);
+    console.log(searchButton);
+    console.log(clearButton);
+
+    const searchName = () => {
+      cardContainer.innerHTML = "";
+      let nameInput = document.getElementById('searchByName').value.toLowerCase();
+      filteredArr = pokemonArray.filter(item => item['name'] == nameInput);
+
+      pokemonArray(filteredArr);
+
+      console.log(nameInput);
+    }
+
+    const searchType = () => {
+      cardContainer.innerHTML = "";
+      let typeInput = document.getElementById('searchByType')
+      filteredArr = pokemonArray.filter(item => item['type'] == typeInput);
+
+      pokemonArray(filteredArr);
+    }
+
+    searchButton.addEventListener('click', searchName);
+    
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+    
 
